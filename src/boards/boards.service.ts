feb044ccfd1b833cf0713688cee7ba89b49439be
ssunnykku <ssunnykku@gmail.com>
2023-03-29 23:56:58 +0900

    파이프를 이용한 유효성 체크

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -29,12 +29,18 @@ export class BoardsService {
 
   //  id로 특정 게시물 가져오기(parameter 이용)
   getBoardById(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const found = this.boards.find((board) => board.id === id);
+    if (!found) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+    return found;
   }
 
   //   id로 특정 게시물 지우기
   deleteBoard(id: string): void {
-    this.boards = this.boards.filter((board) => board.id !== id);
+    // 없는 게시물을 지우려 할 때 결과값 처리
+    const found = this.getBoardById(id);
+    this.boards = this.boards.filter((board) => board.id !== found.id);
   }
 
   //   특정 게시물의 상태 업데이트
